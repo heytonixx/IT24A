501b215f0a3c5fb990ac032e9515d6569dee39f7
xhinsade <nekochii57@gmail.com>
2024-10-04 14:19:24 +0800

    added addMarker

--- applet.js
+++ applet.js
@@ -4,10 +4,14 @@ class LeafletMap {
         this.map = L.map(containerId).setView(center, zoom);
         this.initTileLayer();
     }
-    
+
     initTileLayer() {
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(this.map);
+    }
+addMarker(lat, lng, message) {
+        const marker = L.marker([lat, lng]).addTo(this.map);
+        marker.bindPopup(message);
     }
