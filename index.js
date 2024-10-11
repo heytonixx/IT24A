5f02c754c3e0aa9230b4807d4ceb7ca0b4193a39
heytonixx <dongkiecanete0@gmail.com>
2024-10-11 19:46:08 +0800

    added foreach

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ class CardSearch {
         this.cards.forEach(card => {
             const title = card.querySelector('.card-title').textContent.toLowerCase();
             const text = card.querySelector('.card-text').textContent.toLowerCase();
+            
 
             if (title.includes(searchTerm) || text.includes(searchTerm)) {
                 card.closest('.col-sm-4').style.display = '';
