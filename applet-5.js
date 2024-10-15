acd45899dd2040f4c761167e2a230dd8be62332f
heytonixx <dongkiecanete0@gmail.com>
2024-10-15 10:42:08 +0800

    added fetchWeather

--- applet-5.js
+++ applet-5.js
@@ -1,2 +1,13 @@
 class WeatherApp{
 }
+
+class WeatherService extends WeatherApp {
+
+    constructor() {
+        super();
+        this.apiKey = '';
+    }
+    async fetchWeather() {
+        const city = this.cityInput.value;
+        this.apiKey = this.APIKeyInput.value.trim(); 
+    
