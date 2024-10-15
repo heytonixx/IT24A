7a11a19807e67ae3f25842baff21c04bef800fd7
heytonixx <dongkiecanete0@gmail.com>
2024-10-15 10:42:43 +0800

    added alert

--- applet-5.js
+++ applet-5.js
@@ -10,4 +10,8 @@ class WeatherService extends WeatherApp {
     async fetchWeather() {
         const city = this.cityInput.value;
         this.apiKey = this.APIKeyInput.value.trim(); 
-    
+        
+        if (!this.apiKey) {
+            alert('Please enter your API key.');
+            return;
+        }
